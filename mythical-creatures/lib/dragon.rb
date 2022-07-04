170ca76f641e08c553d26134240e5673a8d34261
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-04 17:47:37 -0600

    Dragon test

---
+++ mythical-creatures/lib/dragon.rb
@@ -0,0 +1,17 @@
+class Dragon
+  attr_reader :name, :color, :rider
+  def initialize(name, color, rider)
+    @name = name
+    @color = color
+    @rider = rider
+    @times_eaten = 0
+  end
+
+  def hungry?
+    @times_eaten < 3
+  end
+
+  def eat
+    @times_eaten += 1
+  end
+end
